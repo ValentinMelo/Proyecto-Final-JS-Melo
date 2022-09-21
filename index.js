ea9345da03063b6e1a46b1ffdf55612bf2d97cdb
Vale Melo <valemelo18@gmail.com>
2022-09-21 00:28:32 -0300

    Cambios

--- index.js
+++ index.js
@@ -1,76 +1,86 @@
-let boton = document.getElementById('boton')
+const boton = document.getElementById('boton')
+const capital = document.getElementById('monto')
+const numeroCuotas = document.getElementById('cuotas')
+const ingresos = document.getElementById('ingresos')
+const nombre = document.getElementById('nombre')
+const apellido = document.getElementById('apellido')
+const divSaludo = document.querySelector('#divSaludo')
+const divPrestamo = document.querySelector('#divPrestamo')
+const maximoCapital = 5000
+const maximoCuotas = 12
 
-boton.onclick = (e)=>{
-    let capital = document.getElementById('monto').value
-    let numeroCuotas = document.getElementById('cuotas').value
-    let ingresos = document.getElementById('ingresos').value
-    let maximoCapital = 5000
-    let maximoCuotas = 12
-    e.preventDefault()
 
-    function montoCuota(prestamo){
-        let interes = 0
-        if(numeroCuotas<=6){
-            interes = 2
-        } else {
-            interes = 5
-        }
-        let valorCuota = (prestamo/numeroCuotas)*(1+(interes/100))
-        return valorCuota
+function crearSaludo (user){
+    const saludarTitulo = document.createElement('h2');
+    saludarTitulo.innerText = `Hola ${user.nombre} ${user.apellido}, te otorgaremos un préstamo con las siguientes características:`;
+    divSaludo.append(saludarTitulo)
+}
+
+function montoCuota(prestamo){
+    let interes = 0
+    const datos1 = {
+        cuotasElegidas: numeroCuotas.value
     }
-
-    
-    let prestamoPedido = [capital, montoCuota(capital)]
-    let cobrable = 0
-
-    if (capital<=maximoCapital && numeroCuotas<=6 && (ingresos>capital || montoCuota(capital)<ingresos)){
-        for(let i=1;i<=numeroCuotas;i++){
-                console.log('Cuota ',i,': ',montoCuota(capital))
-                cobrable = 1
-        }
-    } else if(capital<=maximoCapital && numeroCuotas>6 && numeroCuotas<=maximoCuotas && (ingresos>capital || montoCuota(capital)<ingresos)) {
-        for(let i=1;i<=numeroCuotas;i++){
-            console.log('Cuota ',i,': ',montoCuota(capital))
-            cobrable = 1
-    }
-    } else if(capital<=maximoCapital && numeroCuotas<=12 && montoCuota(capital)>ingresos){
-        alert ('No cumple con los requisitos crediticios para cumplir con la cuota mensual')
+    if(datos1.cuotasElegidas<=6){
+        interes = 2
     } else {
-        alert ('Requisitos: máximo de capital 5.000 USD y máximo de cuotas 12')
+        interes = 5
     }
+    let valorCuota = (prestamo/datos1.cuotasElegidas)*(1+(interes/100))
+    return valorCuota
+}
 
-
-
-    while(cobrable===1){
-        let decision = parseInt(prompt(`Te hemos otorgado el siguiente préstamo: capital total de ${prestamoPedido[0]} y cuotas mensuales fijas de ${prestamoPedido[1]}. Deseas obtener otro préstamo? 1.Si 2.No`))   
-        if (decision===1 && ingresos>capital){      
-            let montoRestante = (maximoCapital - capital)
-            alert(`Te otorgaremos un último préstamo de ${montoRestante} y con una cuota mensual de ${montoCuota(montoRestante)}`)
-            prestamoPedido.push(montoRestante, montoCuota(montoRestante))
-            alert(`Tus préstamos aprobados son los siguientes: Préstamo 1 (Capital: ${prestamoPedido[0]} y cuota: ${prestamoPedido[1]}) y Préstamo 2 (Capital: ${prestamoPedido[2]} y cuota: ${prestamoPedido[3]})`)
-        } else if (decision===1 && ingresos>montoCuota(capital)){      
-            let montoRestante2 = ((ingresos - montoCuota(capital))*0.5)
-            alert(`Te otorgaremos un último préstamo de ${montoRestante2} y con una cuota mensual de ${montoCuota(montoRestante2)}`)
-            prestamoPedido.push(montoRestante2, montoCuota(montoRestante2))
-            alert(`Tus préstamos aprobados son los siguientes: Préstamo 1 (Capital: ${prestamoPedido[0]} y cuota: ${prestamoPedido[1]}) y Préstamo 2 (Capital: ${prestamoPedido[2]} y cuota: ${prestamoPedido[3]})`)
-        } else if(decision!==1){       
-            alert('Muchas gracias por pedir su préstamo con nosotros!')
-        }
-        break
+function calculadora(){
+    const datos = {
+        capital: capital.value,
+        numeroCuotas: numeroCuotas.value,
+        ingresos: ingresos.value
     }
-
-
-    const totalCuotas = [prestamoPedido[1], prestamoPedido[3]]
-    totalCuotas.forEach( (datos)=> {
-    const parrafo = document.createElement ('p')
-    parrafo.innerText = `Cuota prestamo: ${datos}`
-    parrafo.className = 'cuota-prestamo'
-    document.body.appendChild(parrafo)
-    const boton = document.createElement('button')
-    boton.innerText = 'Elegir cuenta bancaria desde la cual se debitarán los fondos'
-    document.body.appendChild(boton)
-    })
     
     
+    if (datos.capital<=maximoCapital && datos.numeroCuotas<=6 && (datos.ingresos>datos.capital || montoCuota(datos.capital)<datos.ingresos)){
+        const prestamoParrafo = document.createElement('p')
+        prestamoParrafo.innerText = `Capital: ${datos.capital} Cuota mensual:${montoCuota(datos.capital)}`
+        divPrestamo.append(prestamoParrafo)
+    } else if(datos.capital<=maximoCapital && datos.numeroCuotas>6 && datos.numeroCuotas<=maximoCuotas && (datos.ingresos>datos.capital || montoCuota(datos.capital)<datos.ingresos)) {
+        const prestamoParrafo = document.createElement('p')
+        prestamoParrafo.innerText = `Capital: ${datos.capital} Cuota mensual:${montoCuota(datos.capital)}`
+        divPrestamo.append(prestamoParrafo)
+    } else if(datos.capital<=maximoCapital && datos.numeroCuotas<=12 && montoCuota(datos.capital)>datos.ingresos){
+        const prestamoParrafo = document.createElement('p')
+        prestamoParrafo.innerText = `No cumple con los requisitos crediticios para cumplir con la cuota mensual`
+        divPrestamo.append(prestamoParrafo)
+    } else {
+        const prestamoParrafo = document.createElement('p')
+        prestamoParrafo.innerText = `Requisitos: máximo de capital 5.000 USD y máximo de cuotas 12`
+        divPrestamo.append(prestamoParrafo)
+    }
+}
+
+
+
+boton.onclick = (e) => {
+    e.preventDefault()
+    
+    // saludo a usuario
+    const usuario = {
+        nombre: nombre.value,
+        apellido: apellido.value,
+    }
+    localStorage.setItem('usuarioStorage', JSON.stringify(usuario))
+    crearSaludo(usuario)
+    calculadora()
+
+    // eliminar form
+    boton.style.display = "none";
+    capital.style.display = "none";
+    numeroCuotas.style.display = "none";
+    ingresos.style.display = "none";
+    nombre.style.display = "none";
+    apellido.style.display = "none";
+
 
 }
+
+
+
